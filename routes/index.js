b31b759b5777d142b7dadcc1b8bb78aef52f34c8
Usama Aslam <usamaaslam4356@gmail.com>
2026-09-21 10:15:42 +0500

    postModel created

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var userModel=require('./users');
+var postModel=require('./post');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
