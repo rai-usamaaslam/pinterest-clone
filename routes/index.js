c40c83fdda5cc31e23ff2a0247d819d2cee8b08b
Usama Aslam <usamaaslam4356@gmail.com>
2026-09-21 11:14:19 +0500

    passport added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var userModel=require('./users');
 var postModel=require('./post');
+const localStrategy=require('passport-local');
+const passport = require('passport');
+passport.authenticate(new localStrategy(userModel.authenticate()));
+
+      
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
